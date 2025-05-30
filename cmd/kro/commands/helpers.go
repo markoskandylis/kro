043241f7704d021237f2a1208f553ee4654fb7ef
Markos Kandylis <markos_kandylis@hotmail.com>
2025-05-30 11:03:19 +0100

    Cleaned up the unused function

--- cmd/kro/commands/helpers.go
+++ cmd/kro/commands/helpers.go
@@ -16,11 +16,8 @@ package commands
 
 import (
 	"context"
-	"fmt"
 
 	krov1alpha1 "github.com/kro-run/kro/api/v1alpha1"
-	kroclient "github.com/kro-run/kro/pkg/client"
-	"github.com/kro-run/kro/pkg/graph"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/scale/scheme"
@@ -34,23 +31,6 @@ func getKubeClient() (client.Client, error) {
 	_ = krov1alpha1.AddToScheme(s)
 	cfg := ctrl.GetConfigOrDie()
 	return client.New(cfg, client.Options{Scheme: s})
-}
-
-func rgdBuilder(rgd krov1alpha1.ResourceGraphDefinition) (*graph.Graph, error) {
-	set, err := kroclient.NewSet(kroclient.Config{})
-	if err != nil {
-		return nil, fmt.Errorf("failed to init kro client: %w", err)
-	}
-	builder, err := graph.NewBuilder(set.RESTConfig())
-	if err != nil {
-		return nil, fmt.Errorf("failed to build graph: %w", err)
-	}
-	graphRuntime, err := builder.NewResourceGraphDefinition(&rgd)
-	if err != nil {
-		return nil, fmt.Errorf("failed to render graph runtime: %w", err)
-	}
-
-	return graphRuntime, nil
 }
 
 func getRGDIfExists(c client.Client, name string) (krov1alpha1.ResourceGraphDefinition, bool, error) {
